f60783e5b4ed674855a7d931a6e9f14ab0c90dcb
Zachary Doucet <zachary.doucet@cn.ca>
2025-08-13 14:14:28 -0600

    added data grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 // src/App.js
 import './App.css';
 import theme from './theme';
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
+import { DataGrid } from '@mui/x-data-grid';
+
+// Constants
+import {
+  columnsCarsProcessed,
+} from './utils/constants';
 
 // Hooks and Utils
 import { useTerminals } from './hooks/useTerminals';
@@ -24,21 +30,33 @@ function App() {
   // Checkbox, Radio Buttons, Capture Button
   const { terminal, thruTrainCars, dwellType, handleTerminalChange, handleCheckboxChange, handleDwellTypeChange } = useCaptureForm();
 
+  const [currentRows, setCurrentRows] = useState([]);
+  const [currentColumns, setCurrentColumns] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   // The final action handler, now clean and focused
-  const handleCapture = () => {
-    captureTrainData({
+  const handleCapture = async () => {
+    setIsLoading(true)
+    setIsVisible(true);
+    
+    const apiResult = await captureTrainData({
       start_date: startDate,
       end_date: endDate,
       dwell_type: dwellType,
       terminal,
       enabled: thruTrainCars ? "1" : "0",
     });
+
+    setCurrentColumns(columnsCarsProcessed)
+    setCurrentRows(apiResult.data || [])
+    setIsLoading(false)
   };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
         <Header />
         
         <Box component="main" sx={{ pt: 6, maxWidth: '1000px', mx: 'auto', px: 1 }}>
@@ -68,6 +86,37 @@ function App() {
             onCapture={handleCapture}
           />
 
+        </Box>
+    
+        <Box sx={{ flexGrow: 1, p: 1, minHeight: 0, justifyContent: 'center', alignItems: 'center', display: isVisible ? 'flex' : 'none' }}> 
+          <div style={{ height: '95%', width: '90%' }}> {/* Set explicit height for DataGrid */}
+            <DataGrid
+              rows={currentRows}
+              columns={currentColumns}
+              getRowId={(row) => row.ID}
+              pageSize={5}
+              // checkboxSelection
+              disableRowSelectionOnClick
+              loading={isLoading}
+              slotProps={{
+                loadingOverlay: {
+                  variant: 'linear-progress', // or 'skeleton', 'linear-progress'
+                  noRowsVariant: 'linear-progress',
+                },
+              }}
+              initialState={{
+                columns: {
+                  columnVisibilityModel: { ID: false } // Hide Fields
+                },
+                // sorting: {
+                //   sortModel: [{ field: 'STN_ID_333_FRM', sort: 'asc' }],
+                // },
+              }}
+              showToolbar
+            />
+          </div>
+          {/* <Button onClick={handleClose} sx={{ mt: 2 }}>Close</Button>
+          <Button onClick={() => handleExport(prevSelection)} sx={{ mt: 2 }} variant="contained" color="primary">Export to Excel</Button> */}
         </Box>
       </Box>
     </ThemeProvider>
